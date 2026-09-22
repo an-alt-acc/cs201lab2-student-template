91eb3e2ba0a97bb2559367ad3711935b863d9f3e
an-alt-acc <134623451+an-alt-acc@users.noreply.github.com>
2026-09-22 12:44:36 +0800

    make fast

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import org.w3c.dom.Node;
+
 public class SinglyLinkedList<E extends Comparable<E>> {
     private Node<E> head = null;
     private Node<E> tail = null;
@@ -99,47 +101,66 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         return sb.toString();
     }
 
+    private static class OrderedNodesEntry<E extends Comparable<E>> implements Comparable<OrderedNodesEntry<E>> {
+        private Node<E> pred;
+        private Node<E> node;
+
+        OrderedNodesEntry(Node<E> pred, Node<E> node) {
+            this.pred = pred;
+            this.node = node;
+        }
+
+        public Node<E> getPred() {
+            return pred;
+        }
+
+        public Node<E> getNode() {
+            return node;
+        }
+
+        public int compareTo(OrderedNodesEntry<E> o) {
+            return getNode().getElement().compareTo(o.getNode().getElement());
+        };
+    }
+
     // write your codes here
     public void swap(){
         if (size <= 1) return;
 
-        HashSet<E> visited = new HashSet<>(size);
-        while (visited.size() < size - 1) {
-            Node<E> min = null;
-            Node<E> minPred = null;
-            Node<E> max = null;
-            Node<E> maxPred = null;
-            Node<E> ptr = head;
-            Node<E> ptrPred = null;
-            while (ptr != null) {
-                if (visited.contains(ptr.getElement())) {
-                    ptrPred = ptr;
-                    ptr = ptr.getNext();
-                    continue;
-                }
-                if (min == null || ptr.getElement().compareTo(min.getElement()) < 0) {
-                    min = ptr;
-                    minPred = ptrPred;
-                }
-                if (max == null || ptr.getElement().compareTo(max.getElement()) > 0) {
-                    max = ptr;
-                    maxPred = ptrPred;
-                }
-                ptrPred = ptr;
-                ptr = ptr.getNext();
-            }
-
+        TreeSet<OrderedNodesEntry<E>> orderedNodes = new TreeSet<>();
+        HashMap<Node<E>, Node<E>> swappedNodes = new HashMap<>();
+        Node<E> ptr = head;
+        Node<E> ptrPred = null;
+        while (ptr != null) {
+            orderedNodes.add(new OrderedNodesEntry<>(ptrPred, ptr));
+            ptrPred = ptr;
+            ptr = ptr.getNext();
+        }
+        
+        do {
+            OrderedNodesEntry<E> minPtr = orderedNodes.removeFirst();
+            OrderedNodesEntry<E> maxPtr = orderedNodes.removeLast();
+            
+            Node<E> min = minPtr.getNode();
+            Node<E> minPred = swappedNodes.getOrDefault(minPtr.getPred(), minPtr.getPred());
             Node<E> minNext = min.getNext();
+
+            Node<E> max = maxPtr.getNode();
+            Node<E> maxPred = swappedNodes.getOrDefault(maxPtr.getPred(), maxPtr.getPred());
             Node<E> maxNext = max.getNext();
+
             if (maxPred == null) head = min; else maxPred.setNext(min);
             min.setNext(maxNext == min ? max : maxNext);
             if (maxNext == null) tail = min;
+
             if (minPred == null) head = max; else minPred.setNext(max);
             max.setNext(minNext == max ? min : minNext);
             if (minNext == null) tail = max;
-            visited.add(min.getElement());
-            visited.add(max.getElement());
-        }
+
+            swappedNodes.put(min, max);
+            swappedNodes.put(max, min);
+            
+        } while (orderedNodes.size() > 1);
     }
    
 }
